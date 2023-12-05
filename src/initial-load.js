7659b32543747513e7bba48fd60b9f69a50e23cb
Suman Sur <sumansur.cs98@gmail.com>
2023-12-05 20:36:26 +0530

    created navbar

--- src/initial-load.js
+++ src/initial-load.js
@@ -6,9 +6,27 @@ const createHeader = ()=>{
     const h1Ele = document.createElement('h1')
     h1Ele.textContent = 'Rollacosta';
     headerEle.appendChild(h1Ele);
+    headerEle.appendChild(createNav());
     
     return headerEle;
 
+}
+
+const createNav = ()=>{
+    const navEle = document.createElement('ul');
+
+    const homeEle = document.createElement('li');
+    homeEle.textContent = 'Home'
+
+    const menuEle = document.createElement('li');
+    menuEle.textContent = 'Menu'
+
+    const contactEle = document.createElement('li');
+    contactEle.textContent = 'Contact'
+
+    navEle.appendChild(homeEle).appendChild(menuEle).appendChild(contactEle);
+
+    return navEle;
 }
 
 const loadInitialPage = () => {
